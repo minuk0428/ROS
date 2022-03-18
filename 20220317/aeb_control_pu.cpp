b1faf30b36c4088ab1d10ab0af11cb8c7f10209a
minuk0428 <101725486+minuk0428@users.noreply.github.com>
2022-03-18 12:04:59 +0900

    Add files via upload

---
+++ 20220317/aeb_control_pu.cpp
@@ -0,0 +1,45 @@
+#include "ros/ros.h"
+#include "std_msgs/Int8.h"
+#include "std_msgs/Bool.h"
+#include "std_msgs/String.h"
+#include "sensor_msgs/Range.h"
+#include "geometry_msgs/Twist.h"
+
+std_msgs::Bool flag_AEB;
+
+void UltraSonarCallback(const sensor_msgs::Range::ConstPtr& msg){
+	
+	ROS_INFO("sonar Range: [%f]",msg->range);
+	
+	if(msg->range <=1.0){
+		
+		ROS_INFO("AEB_Activation!!");
+		flag_AEB.data = true;
+		}
+		else{
+			
+			flag_AEB.data=false;
+			}
+	
+	}
+
+int main(int argc, char **argv){
+	
+	int count = 0;
+	ros::init(argc, argv, "aeb_controller");
+	
+	ros::NodeHandle n;
+	
+	
+	
+	ros::Subscriber sub= n.subscribe("range",1000,UltraSonarCallback);
+	ros::Publisher pub1=n.advertise<std_msgs::Bool>("range1",1000);
+	ros::Rate loop_rate(1);
+	while(ros::ok()){
+		
+		loop_rate.sleep();
+		ros::spinOnce();
+		++count;
+		}
+		return 0;
+	}
